d77dcd009723a70484a5c70ff4b2da57821e3c6f
Roman Hnatiuk <roman.hnatiuk02@gmail.com>
2024-02-07 19:14:24 +0200

    fix

--- try.js
+++ try.js
@@ -1,27 +1,74 @@
-import express from 'express';
-const app = express();
-import fetch from 'node-fetch';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+const http = require('http');
+const querystring = require('querystring');
+const axios = require('axios');
+const { URL } = require('url');
 
-app.use(cookieParser());
-app.use(cors());
-app.listen(process.env.PORT || 5000);
+const server = http.createServer((req, res) => {
+    const { method, url } = req;
+    const parsedUrl = new URL(`http://localhost:4000${url}`);
 
-const CLIENT_KEY = 'awtk9q11ll2kqoe1' // this value can be found in app's developer portal
+    if (method === 'GET' && parsedUrl.pathname === '/oauth') {
+        const csrfState = Math.random().toString(36).substring(2);
+        res.setHeader('Set-Cookie', [`csrfState=${csrfState}; Max-Age=60000`]);
+        const params = {
+            client_key: 'awtk9q11ll2kqoe1',
+            scope: 'user.info.basic,user.info.profile,user.info.stats,video.list',
+            response_type: 'code',
+            redirect_uri: 'https://sportscore.io/',
+            state: csrfState
+        };
+        const url = `https://www.tiktok.com/v2/auth/authorize/?${querystring.stringify(params)}`;
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ url }));
+    } else if (method === 'POST' && parsedUrl.pathname === '/tiktokaccesstoken') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', async () => {
+            try {
+                const { code } = JSON.parse(body);
+                const decode = decodeURI(code);
+                const tokenEndpoint = 'https://open.tiktokapis.com/v2/oauth/token/';
+                const params = {
+                    client_key: 'awtk9q11ll2kqoe1',
+                    client_secret: 'Frrdb0ZJUjCh7hXtEc9VinH1rr6ysnjk',
+                    code: decode,
+                    grant_type: 'authorization_code',
+                    redirect_uri: 'https://sportscore.io/'
+                };
+                const response = await axios.post(tokenEndpoint, querystring.stringify(params), {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Cache-Control': 'no-cache'
+                    }
+                });
+                if (response.data.access_token) {
+                    const allvideosdata = await axios.post('https://open.tiktokapis.com/v2/video/list/?fields=id,title,video_description,duration,cover_image_url,embed_link', {
+                        max_count: 20
+                    }, {
+                        headers: {
+                            Authorization: `Bearer ${response.data.access_token}`,
+                            'Content-Type': 'application/json'
+                        }
+                    });
+                    console.log(allvideosdata.data.data.videos); // Lists all videos of user along with other details
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(allvideosdata.data.data.videos));
+                }
+            } catch (error) {
+                console.error('Error during callback:', error.message);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('An error occurred during the login process.');
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
 
-app.get('/oauth', (req, res) => {
-    const csrfState = Math.random().toString(36).substring(2);
-    res.cookie('csrfState', csrfState, { maxAge: 60000 });
-
-    let url = 'https://www.tiktok.com/v2/auth/authorize/';
-
-    // the following params need to be in `application/x-www-form-urlencoded` format.
-    url += '?client_key=awtk9q11ll2kqoe1';
-    url += '&scope=user.info.basic';
-    url += '&response_type=code';
-    url += '&redirect_uri=https://sportscore.io/';
-    url += '&state=' + csrfState;
-
-    res.redirect(url);
-})
+const PORT = 4000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
